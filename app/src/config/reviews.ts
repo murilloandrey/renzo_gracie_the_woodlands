14a9d2c18a175b6013e3f4fd60a01a44fbbc40fe
Andrey J. Murillo <43352531+murilloandrey@users.noreply.github.com>
2026-07-30 23:51:46 +0000

    Add current Google review details

--- app/src/config/reviews.ts
+++ app/src/config/reviews.ts
@@ -1,3 +1,4 @@
 export const GOOGLE_RATING = 5.0;
-export const GOOGLE_REVIEW_COUNT = 0; // TODO: real count before launch
-export const GOOGLE_REVIEWS_URL = ""; // TODO: Google Maps reviews link
+export const GOOGLE_REVIEW_COUNT = 278;
+export const GOOGLE_REVIEWS_URL =
+  "https://search.google.com/local/reviews?placeid=ChIJCzvxrSc3R4YR9mlfWvCHWow";
